82442d63cd31876371af7065eab383c440f7d1d5
Saad Mahmood <saadsmahmood@gmail.com>
2021-04-21 03:09:38 -0400

    Login stuff

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+<body>
+
+    <form id="Login" action="index.html" method="POST">
+        <input type="text" name="username" placeholder="Username"/>
+        <input type="submit"/>
+        <input type="submit" name="guest" value="Sign in as Guest" />
+    </form>
+    
+</body>
+</html>
